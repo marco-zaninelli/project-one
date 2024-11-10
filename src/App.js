21957e8e13f9609ba8c7a0887814ee017d962130
Marco Zaninelli <marco@zaninelli.me>
2024-11-10 16:27:42 +1100

    Checklist project finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Checklist from "./checklist";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    const initialItems = [
+        {text: "Buy groceries for the week", completed: false},
+        {text: "Prepare presentation for Monday meeting", completed: true},
+        {text: "Call the electrician to fix kitchen lights", completed: false},
+        {text: "Finish reading the latest book", completed: false},
+        {text: "Schedule annual health check-up", completed: true},
+    ];
+
+    return (
+        <Checklist initialChecklist={initialItems} />
+    );
 }
 
 export default App;
